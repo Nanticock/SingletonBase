4ced204ecf095b901b5881a1ed4087375cfd1a07
Mostafa Nanticock <47303284+MostafaNanticock@users.noreply.github.com>
2026-03-13 08:18:56 +0200

    Update SingletonBase.h

--- include/PM/SingletonBase.h
+++ include/PM/SingletonBase.h
@@ -91,3 +91,46 @@ inline T **PM::SingletonBase<T>::getInstanceRef() noexcept
 }
 
 #endif // SINGLETONBASE_H
+
+#ifndef SCOPEDSINGLETONSTATE_H
+#define SCOPEDSINGLETONSTATE_H
+
+namespace PM
+{
+template <typename T>
+class ScopedSingletonState
+{
+public:
+    ScopedSingletonState() noexcept;
+    ~ScopedSingletonState() noexcept;
+
+    ScopedSingletonState(ScopedSingletonState<T> &&other) noexcept = delete;
+    ScopedSingletonState(const ScopedSingletonState<T> &other) noexcept = delete;
+
+    T &instance() const noexcept;
+
+private:
+    T *m_lastInstance;
+    mutable T m_instance;
+};
+} // namespace PM
+
+template <typename T>
+inline PM::ScopedSingletonState<T>::ScopedSingletonState() noexcept : m_lastInstance(nullptr)
+{
+    m_lastInstance = static_cast<T *>(T::setInstance(&m_instance));
+}
+
+template <typename T>
+inline PM::ScopedSingletonState<T>::~ScopedSingletonState() noexcept
+{
+    T::setInstance(m_lastInstance);
+}
+
+template <typename T>
+inline T &PM::ScopedSingletonState<T>::instance() const noexcept
+{
+    return m_instance;
+}
+
+#endif // SCOPEDSINGLETONSTATE_H
